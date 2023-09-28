96eec3aa556b502aeb6da4fd3662c89b9dda49a3
flora <anastasiiaflorian@gmail.com>
2023-09-28 22:39:33 +0300

    Правки внесены

--- test_google.py
+++ test_google.py
@@ -2,12 +2,10 @@ from selene.support.shared import browser
 from selene import be, have
 
 def test_search_1():
-    browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser ...'))
 
 
 def test_search_2():
-    browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('serdtfyguhijnokmljnkml').press_enter()
-    browser.element('[id="search"]').should(have.text('Результатов: примерно 0'))
+    browser.element('[id="result-stats"]').should(have.text('Результатов: примерно 0'))
